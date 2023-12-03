2fbf6516b478433542daba196a011f838a1fa1fe
Daniel Evora <daniel.evora764@gmail.com>
2023-12-02 16:26:47 -0800

    add basic api implementation and gitignore

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,28 +5,27 @@ namespace EmployeeApi.Models;
 
 public class Employee
 {
-  [Required]
-  public Guid EmployeeId { get; set; }
+  public string? EmployeeId { get; set; }
 
   [Required]
   [StringLength(50)]
-  public string FirstName { get; set; }
+  public required string FirstName { get; set; }
 
   [Required]
   [StringLength(50)]
-  public string LastName { get; set; }
+  public required string LastName { get; set; }
 
   [Required]
   [EmailAddress]
-  public string Email { get; set; }
+  public required string Email { get; set; }
 
   [Required]
   [Phone]
-  public string PhoneNumber { get; set; }
+  public required string PhoneNumber { get; set; }
 
   [Required]
   [StringLength(100)]
-  public string Address { get; set; }
+  public required string Address { get; set; }
 
   [Required]
   [DataType(DataType.Date)]
@@ -36,11 +35,11 @@ public class Employee
 
   [Required]
   [StringLength(50)]
-  public string Department { get; set; }
+  public required string Department { get; set; }
 
   [Required]
   [StringLength(50)]
-  public string Status { get; set; }
+  public required string Status { get; set; }
 }
 
 public class DateNotInTheFutureAttribute : ValidationAttribute
